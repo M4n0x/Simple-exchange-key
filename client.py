ec1d2f2a4590e4ecca0d549104c5d06841843296
Luca Laissue <luca.laissue@he-arc.ch>
2021-04-22 14:44:24 +0200

    Client WIP

--- client.py
+++ client.py
@@ -1,22 +1,58 @@
 #!/usr/bin/env python3
 
 import socket
+import enum
+import time
+import rsa
+
+pub, priv = rsa.newkeys(1024)
+
+class ConnectionState(enum.Enum):
+    CONNECTING = 0
+    STARTED = 1
+    WAITING_KEY = 2
+    NORM = 3
+    ENDED = 4
+
+state = ConnectionState.CONNECTING
 
 ClientSocket = socket.socket()
 host = '127.0.0.1'
 port = 1233
 
-print('Waiting for connection')
-try:
-    ClientSocket.connect((host, port))
-except socket.error as e:
-    print(str(e))
+cryptKey = ""
 
-Response = ClientSocket.recv(1024)
 while True:
-    Input = input('Say Something: ')
-    ClientSocket.send(str.encode(Input))
-    Response = ClientSocket.recv(1024)
-    print(Response.decode('utf-8'))
+    # Connection
+    if state == ConnectionState.CONNECTING:
+        try:
+            print(f"Connecting to {host}:{port}")
+            ClientSocket.connect((host, port))
+            print(ClientSocket.recv(1024))
+            print("Connected")
+            state = ConnectionState.STARTED
+        except socket.error as e:
+            time.sleep(1)
+            pass
 
-ClientSocket.close()
+    # Send public key
+    elif state == ConnectionState.STARTED:
+        print("Sending pub key")
+        pubStr = str(pub.save_pkcs1(), "utf-8")
+        ClientSocket.send(str.encode(f"pub:{pubStr}"))
+        state = ConnectionState.WAITING_KEY
+
+    # Receive symetric key
+    elif state == ConnectionState.WAITING_KEY:
+        message = str(ClientSocket.recv(1024), "utf-8").split(":")
+        print(message)
+        if not message[0] == "key":
+            state = ConnectionState.ENDED
+        else:
+            cryptKey = message[1]
+            state = ConnectionState.NORM
+
+    elif state == ConnectionState.ENDED:
+        ClientSocket.close()
+        break
+
